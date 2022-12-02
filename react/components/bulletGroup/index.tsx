5b7521b23da99e492c789cf3d670a903e2a1110b
unknown <elgatoban@gmail.com>
2022-12-01 20:03:54 -0500

    feat final de componente de list context de bullets
    se agregaron estilos necesarios para mobile

--- react/components/bulletGroup/index.tsx
+++ react/components/bulletGroup/index.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren }from "react"
 import {BulletsSchema} from "./BulletTypes"
 import {getBulletsAsTSXList} from "./modules/bulletsAsList"
 import {useListContext, ListContextProvider} from "vtex.list-context"
+import {useCssHandles} from "vtex.css-handles"
 import { useDevice } from "vtex.device-detector"
 
 export interface BulletGroupProps{
@@ -11,19 +12,19 @@ const BulletGroup =({bullet,children}: PropsWithChildren<BulletGroupProps>)=>{
     const {isMobile}=useDevice();
     const { list }= useListContext() || []
     
-    console.log(bullet)
 
     const bulletsContent = getBulletsAsTSXList(bullet)
     const newListContextValue= list.concat(bulletsContent)
-    if (false){
-        console.log(children, list);
-        
-    }
+    
+    const CSS_HANDLES =["bullet__container"]
+    const handles = useCssHandles(CSS_HANDLES)
+
     return(
         <ListContextProvider list={newListContextValue}>
            { isMobile ? 
-            children : 
-            <div>{bulletsContent}</div>
+            <div className={handles.bullet__container}>{bulletsContent}</div>
+            : 
+            children
             }
         </ListContextProvider>
     )
